aa35b61df0f46bdcc172e7ed293aa76b2e4feb28
avc285 <avc285@gmail.com>
2023-02-18 11:38:09 +0000

    create boilerplate functionality

--- chess-knight-problem/chess-knight.js
+++ chess-knight-problem/chess-knight.js
@@ -1,6 +1,16 @@
 // create multidimensional array for board
+const chessboard = [];
+for (let i = 0; i < 8; ++i) {
+    const boardRow = [];
+    for (let j = 0; j < 8; ++j) {
+        boardRow.push("");
+    }
+    chessboard.push(boardRow);
+}
+
+drawBoard(chessboard);
+
 // create function to generate coordinates for checking
-// create function to draw and fill in chess board (or split into two functions)
 // set starting coords
 // create recursive function
 // loop through possible coords (if none, return)
@@ -8,3 +18,21 @@
 // invoke function again
 // unset value immediately after
 // draw board
+
+// create function to draw and fill in board
+function drawBoard(chessboard) {
+    const htmlBoard = document.getElementById("chessboard");
+    let isBlack = false;
+    chessboard.forEach((boardRow, y) => {
+        isBlack = isBlack ? false : true;
+        const newRow = document.createElement("tr");
+        boardRow.forEach((cell, x) => {
+            const newCell = document.createElement("td");
+            newCell.innerText = chessboard[y][x];
+            newCell.className = isBlack ? "black" : null;
+            newRow.appendChild(newCell);
+            isBlack = isBlack ? false : true;
+        });
+        htmlBoard.appendChild(newRow);
+    });
+}
