744a2855f50170e5e371fed8aa5561877fe1cbc4
avc285 <avc285@gmail.com>
2023-02-18 11:16:32 +0000

    plan approach

---
+++ chess-knight-problem/chess-knight.js
@@ -0,0 +1,10 @@
+// create multidimensional array for board
+// create function to generate coordinates for checking
+// create function to draw and fill in chess board (or split into two functions)
+// set starting coords
+// create recursive function
+// loop through possible coords (if none, return)
+// if possible to go, set a value to that coordinate
+// invoke function again
+// unset value immediately after
+// draw board
